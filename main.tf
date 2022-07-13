e5f2adcab61f54d0ba41ea3ba9de19ef01829dc3
tribalrehab <tribalrehab@gmail.com>
2022-07-13 13:47:23 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ variable aws_access_key_id{}
 variable aws_secret_access_key{}
 
 provider "aws" {
-	profile="default"
 	region="us-east-1"
+	access_key = var.aws_access_key_id
+	secret_key = var.aws_secret_access_key
 }
 
 resource "aws_instance" "mywebserver" {
